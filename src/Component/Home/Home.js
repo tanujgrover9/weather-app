fb76b31942210bb81beae1f82b869240d69279cc
tanujgrover9 <tanujgrover07@gmail.com>
2024-01-24 13:23:19 +0530

    first commit

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,16 +1,16 @@
-import React, { useState } from 'react'
-import './Home.css'
-import {WEATHER_API_URL, WEATHER_API_KEY} from '../Api'
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import "./Home.css";
+import { WEATHER_API_URL, WEATHER_API_KEY } from "../Api";
+import { useNavigate } from "react-router-dom";
 
-const Home = ({SetWeatherData}) => {
+const Home = ({ SetWeatherData }) => {
   const [city, setcity] = useState("");
-  const [LatLon , SetLatLon] = useState({});
+  const [LatLon, SetLatLon] = useState({});
   const navigate = useNavigate();
 
-  const handlesubmit= async (e)=>{
+  const handlesubmit = async (e) => {
     e.preventDefault();
-    
+
     try {
       const data = await fetch(
         `${WEATHER_API_URL}&q=${city}&appid=${WEATHER_API_KEY}`
@@ -18,18 +18,15 @@ const Home = ({SetWeatherData}) => {
       const result = await data.json();
       SetWeatherData(result);
       if (result.cod === 200) {
-      navigate("/weather");
-      }
-      else if(result.cod == 404)
-      alert("Error City Not Found")
+        navigate("/weather");
+      } else if (result.cod == 404) alert("Error City Not Found");
     } catch (error) {
       console.error("Error fetching weather data:", error);
       alert("Error fetching weather data:", error.message);
-    } 
-  }
+    }
+  };
 
-  const GetLocation =async ()=>{
-
+  const GetLocation = async () => {
     navigator.geolocation.getCurrentPosition((position) => {
       const { latitude, longitude } = position.coords;
       SetLatLon({ latitude, longitude });
@@ -42,34 +39,39 @@ const Home = ({SetWeatherData}) => {
         const result = await data.json();
         SetWeatherData(result);
         if (result.cod === 200) {
-        navigate("/weather");
+          navigate("/weather");
         }
       } catch (error) {
         console.error("Error fetching weather data:", error);
         alert("Error fetching weather data:", error);
-      } 
+      }
     } else {
       alert("Allow device location and try again!");
     }
-
-  }
-
+  };
 
   return (
-    <div className='Input-card'>
-      <h4 className='title'>Weather App</h4>
-      <hr/>
-      <form className='input my-3' onSubmit={handlesubmit}>
-        <input type='text' placeholder='Please Enter City Name' value={city} onChange={(e)=> setcity(e.target.value)}/>
+    <div className="Input-card">
+      <h4 className="title">Weather App.</h4>
+      <hr />
+      <form className="input my-3" onSubmit={handlesubmit}>
+        <input
+          type="text"
+          placeholder="Please Enter City Name"
+          value={city}
+          onChange={(e) => setcity(e.target.value)}
+        />
       </form>
       <div className="or">
-          <hr className="hrLine" />
-          <p style={{ padding: "0 10px", margin: "0.5em 0" }}>or</p>
-          <hr className="hrLine" />
-        </div>
-      <button className='btn btn-primary' onClick={GetLocation}>Get Device Location</button>
+        <hr className="hrLine" />
+        <p style={{ padding: "0 10px", margin: "0.5em 0" }}>or</p>
+        <hr className="hrLine" />
+      </div>
+      <button className="btn btn-primary" onClick={GetLocation}>
+        Get Device Location
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
